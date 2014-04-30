22a7b982e24f1c5c57c0050c25f30789043f2cdb
jmatta1 <jmatta1@nd.edu>
2014-04-30 14:57:47 -0400

    starting to make the calibration window and dialog system

---
+++ CalDialog.h
@@ -0,0 +1,14 @@
+#ifndef CAL_DIALOG_H
+#define CAL_DIALOG_H
+
+class CalibrationDialog
+{
+	RQ_OBJECT("CalibrationDialog")
+public:
+
+private:
+	//main window
+	TGMainFrame *mainWindow;
+};
+
+#endif
---
+++ CalWindow.h
@@ -0,0 +1,14 @@
+#ifndef CAL_WINDOW_H
+#define CAL_WINDOW_H
+
+class CalibrationWindow
+{
+	RQ_OBJECT("CalibrationWindow")
+public:
+
+private:
+	//main window
+	TGMainFrame *mainWindow;
+};
+
+#endif
