d6033dd84a93c1708fe3bfe766f6141b84d54513
jmatta1 <jmatta1@nd.edu>
2014-08-13 19:02:41 -0400

    lots of developement, no here more as a template for setting things up, the relevant functionality has been moved into CalWindow

--- assignmentDialog.h
+++ assignmentDialog.h
@@ -1,9 +1,6 @@
 #ifndef ADJUST_DIALOGS_H
 #define ADJUST_DIALOGS_H
 
-char* GPNameList[] = {"H", "P", "W"};
-char* PBNameList[] = {"a ", "b ", "c ", "d "};
-
 #include<sstream>
 #include<iostream>
 using std::ostringstream;
@@ -14,88 +11,172 @@ using std::endl;
 #include<TGLabel.h>
 #include<TGNumberEntry.h>
 #include<TGButton.h>
+#include<TGTab.h>
+#include<TGClient.h>
+#include<TGCanvas.h>
+#include<TGComboBox.h>
 #include<RQ_OBJECT.h>
 
-class ParamAdjustmentDialog
+#include"guiSupport.h"
+#include"MatrixFrame.h"
+
+class AssignmentDialog
 {
-	RQ_OBJECT("ParamAdjustmentDialog");
+	RQ_OBJECT("AssignmentDialog");
 public:
-	ParamAdjustmentDialog(const TGWindow* parent, UInt_t width, UInt_t height);
-	~ParamAdjustmentDialog();
+	AssignmentDialog(const TGWindow* parent, UInt_t width, UInt_t height);
+	~AssignmentDialog();
 	
-	void initWithParams(int numPeaks, int nP, double* vals);
+	void setStates(StateData* sts, int numSt);
+	void addFit(const FitData& fitDat){}
 	
-	void updateParams(double* values);
-	void retrieveData(double* values, double* lowerBnds, double* upperBnds, bool* useBnds);
-	
-	void show();
-	void hide();
+	void assignStates(){}
+	void fitStates(){}
+	void removeFit(){}
 	
 private:
+	
+	int numStates;
+	StateData* states;
+	int numAssignments;
+	StateFit* assignments;
+	int numFits;
+	FitData* fits;
 
-	void clearPrevious();	
-	
-	int numParams;
-	
 	//main window
 	TGMainFrame *mainWindow;
-	TGVerticalFrame* mainOrganizer; //the organizer for all the sub frames
-	TGHorizontalFrame* labelFrame; //the frame with the list of labels for columns
-	TGVerticalFrame* paramOrganizer; //the organizer for all the sub frames
-	TGHorizontalFrame** paramFrames;//the list of frames for parameters one per parameter
-	
-	//column labels
-	TGLabel* paramColLbl;
-	TGLabel* lbColLbl;
-	TGLabel* valColLbl;
-	TGLabel* ubColLbl;
-	TGLabel* bcbColLbl;
-	
-	//row by row items
-	TGLabel** paramNames;//labels for parameter names
-	TGNumberEntry** lowerBounds;//number entries for parameter lower bounds
-	TGNumberEntry** paramValues;//number entries for parameter starting values
-	TGNumberEntry** upperBounds;//number entries for parameter upper bounds
-	TGCheckButton** useBounds;//buttons indicating the use of bounds for a parameter
+	TGVerticalFrame* overallOrg;
+	TGTab *tabFrame;
+	TGCanvas* assignCanvas;
+	TGHorizontalFrame* buttonContainer;
+	//frames for holding the contents of individual tabs
+	TGCompositeFrame* assignTabFrame;
+	TGCompositeFrame* fitDataTabFrame;
+	TGCompositeFrame* predictionTabFrame;
+	//frames for holding the contents of the TGCanvas
+	TGCompositeFrame* assignContainer;
+	//frames for making labels in assign container
+	TGHorizontalFrame* assignHoriz;
+	TGVerticalFrame** assignVert;
+	TGLabel** colLabels;
+	TGTextEntry** exEntry;
+	TGTextEntry** fpmEntry;
+	TGTextEntry** calfpmEntry;
+	TGTextEntry** aFitEntry;
+	TGTextEntry** difEntry;
+	TGComboBox** fitBox;
+	TGTextButton** assignButtons;
+	TGTextButton** unAssignButtons;
+	//buttons and other stuff
+	TGTextButton *assignButton;
+	TGTextButton *fitButton;
+	TGNumberEntry* polOrder;
+	TGLabel* polOrdLbl;
+	TGTextButton *rmvFitButton;
+	TGComboBox* fitsBox;
 	
 };
 
-ParamAdjustmentDialog::ParamAdjustmentDialog(const TGWindow* parent, UInt_t width, UInt_t height)
+AssignmentDialog::AssignmentDialog(const TGWindow* parent, UInt_t width, UInt_t height)
 {
-	numParams=0;
-	paramFrames=NULL;
-	paramNames=NULL;
-	lowerBounds=NULL;
-	paramValues=NULL;
-	upperBounds=NULL;
-	useBounds=NULL;
+	//create the high level stuff
+	mainWindow = new TGMainFrame(parent, width, height);
+	overallOrg = new TGVerticalFrame(mainWindow, 10, 10);
+	tabFrame = new TGTab(overallOrg, 10, 10);
+	assignTabFrame = new TGCompositeFrame(tabFrame, 10, 10);
+	fitDataTabFrame = new TGCompositeFrame(tabFrame, 10, 10);
+	predictionTabFrame = new TGCompositeFrame(tabFrame, 10, 10);
+	assignCanvas = new TGCanvas(assignTabFrame, 10, 10);
+	buttonContainer = new TGHorizontalFrame(overallOrg, 10, 10);
 	
-	//create the main window for the gui
-	mainWindow = new TGMainFrame(parent,width,height);
-	//create the main organizer
-	mainOrganizer = new TGVerticalFrame(mainWindow, 10, 10);
-	//create the column label frame
-	labelFrame = new TGHorizontalFrame(mainOrganizer, 10, 10);
-	//create the parameter frame organizer
-	paramOrganizer = new TGVerticalFrame(mainOrganizer, 10, 10);
+	//create and assign canvas containers
+	assignContainer = new TGCompositeFrame(assignCanvas->GetViewPort(),50,50);
+	assignContainer->SetCleanup(kDeepCleanup);
+	assignCanvas->GetViewPort()->SetCleanup(kDeepCleanup);
+	assignCanvas->SetContainer(assignContainer);
 	
-	//create the column labels
-	paramColLbl = new TGLabel(labelFrame, "Name");
-	lbColLbl = new TGLabel(labelFrame, "Lower Bound");
-	valColLbl = new TGLabel(labelFrame, "Param Value");
-	ubColLbl = new TGLabel(labelFrame, "Upper Bound");
-	bcbColLbl = new TGLabel(labelFrame, "Apply Bounds?");
-	labelFrame->AddFrame(paramColLbl, new TGLayoutHints(kLHintsNormal,5,5,3,4) );
-	labelFrame->AddFrame(lbColLbl, new TGLayoutHints(kLHintsNormal,5,5,3,4) );
-	labelFrame->AddFrame(valColLbl, new TGLayoutHints(kLHintsNormal,5,5,3,4) );
-	labelFrame->AddFrame(ubColLbl, new TGLayoutHints(kLHintsNormal,5,5,3,4) );
-	labelFrame->AddFrame(bcbColLbl, new TGLayoutHints(kLHintsNormal,5,5,3,4) );
+	//create the column name labels for the assignment tab
+	assignHoriz = new TGHorizontalFrame(assignContainer);
+	assignVert = new TGVerticalFrame*[8];
+	for(int i=0; i<8; ++i)
+	{
+		assignVert[i] = new TGVerticalFrame(assignHoriz, 10, 10);
+		
+	}
 	
-	mainOrganizer->AddFrame(labelFrame, new TGLayoutHints(kLHintsNormal,5,5,3,4) );
-	mainOrganizer->AddFrame(paramOrganizer, new TGLayoutHints(kLHintsNormal,5,5,3,4) );
+	colLabels = new TGLabel*[7];
+	colLabels[0] = new TGLabel(assignVert[0], "Ex");
+	assignVert[0]->AddFrame(colLabels[0], new TGLayoutHints( kLHintsCenterX ,1,1,1,1));
 	
-	mainWindow->AddFrame(mainOrganizer, new TGLayoutHints(kLHintsNormal,5,5,3,4) );
+	colLabels[1] = new TGLabel(assignVert[1], "Calc P_fp");
+	assignVert[1]->AddFrame(colLabels[1], new TGLayoutHints( kLHintsCenterX ,1,1,1,1));
 	
+	colLabels[2] = new TGLabel(assignVert[2], "Cal P_fp");
+	assignVert[2]->AddFrame(colLabels[2], new TGLayoutHints( kLHintsCenterX ,1,1,1,1));
+	
+	colLabels[3] = new TGLabel(assignVert[3], "Assigned Cent.");
+	assignVert[3]->AddFrame(colLabels[3], new TGLayoutHints( kLHintsCenterX ,1,1,1,1));
+	
+	colLabels[4] = new TGLabel(assignVert[4], "Difference");
+	assignVert[4]->AddFrame(colLabels[4], new TGLayoutHints( kLHintsCenterX ,1,1,1,1));
+	
+	colLabels[5] = new TGLabel(assignVert[5], "Select Fit");
+	assignVert[5]->AddFrame(colLabels[5], new TGLayoutHints( kLHintsCenterX ,1,1,1,1));
+	
+	colLabels[6] = new TGLabel(assignVert[6], "Assign Buttons");
+	assignVert[6]->AddFrame(colLabels[6], new TGLayoutHints( kLHintsCenterX ,1,1,1,1));
+	
+	colLabels[7] = new TGLabel(assignVert[7], "Unassign Buttons");
+	assignVert[7]->AddFrame(colLabels[7], new TGLayoutHints( kLHintsCenterX ,1,1,1,1));
+	
+	for(int i=0; i<8; ++i)
+	{
+		assignHoriz->AddFrame(assignVert[i], new TGLayoutHints( kLHintsTop | kLHintsCenterX, 5, 5, 5, 5));
+	}
+	
+	assignContainer->AddFrame(assignHoriz, new TGLayoutHints(kLHintsNormal, 1, 1, 1, 1));
+	
+	//add the canvases to the frames
+	assignTabFrame->AddFrame(assignCanvas, new TGLayoutHints(kLHintsExpandX | kLHintsExpandY,5,5,3,4));
+	
+	//create the tabs
+	tabFrame->AddTab("State/Fit Assignment", assignTabFrame);
+	tabFrame->AddTab("Fit Information", fitDataTabFrame);
+	tabFrame->AddTab("State Positions", predictionTabFrame);
+	
+	//add the tab frame to the overall frame
+	overallOrg->AddFrame(tabFrame, new TGLayoutHints(kLHintsExpandX | kLHintsExpandY,5,5,3,4));
+	
+	//create the buttons and such for the button container
+	assignButton = new TGTextButton(buttonContainer,"Assign States");
+	assignButton->Connect("Clicked()","AssignmentDialog",this,"assignStates()");
+	fitButton = new TGTextButton(buttonContainer,"Do Fitting");
+	fitButton->Connect("Clicked()","AssignmentDialog",this,"fitStates()");
+	polOrder = new TGNumberEntry( buttonContainer, 1.0, 5, -1, TGNumberFormat::kNESInteger,
+			TGNumberFormat::kNEAPositive, TGNumberFormat::kNELLimitMinMax, 1, 2);
+	polOrdLbl = new TGLabel(buttonContainer, "Fit Ord");
+	rmvFitButton = new TGTextButton(buttonContainer,"Remove Fit");
+	rmvFitButton->Connect("Clicked()","AssignmentDialog",this,"removeFit()");
+	fitsBox = new TGComboBox(buttonContainer);
+	fitsBox->AddEntry("Stored Fits",0);
+	fitsBox->Select(0);
+	fitsBox->Resize(100,20);
+	
+	//add the buttons to their frame
+	buttonContainer->AddFrame(assignButton, new TGLayoutHints(kLHintsNormal,5,5,3,4));
+	buttonContainer->AddFrame(polOrdLbl, new TGLayoutHints(kLHintsNormal,5,5,3,4));
+	buttonContainer->AddFrame(polOrder, new TGLayoutHints(kLHintsNormal,5,5,3,4));
+	buttonContainer->AddFrame(fitButton, new TGLayoutHints(kLHintsNormal,5,5,3,4));
+	buttonContainer->AddFrame(fitsBox, new TGLayoutHints(kLHintsNormal,5,5,3,4));
+	buttonContainer->AddFrame(rmvFitButton, new TGLayoutHints(kLHintsNormal,5,5,3,4));
+	
+	//add the button container to the overall organizer
+	overallOrg->AddFrame(buttonContainer, new TGLayoutHints(kLHintsBottom,5,5,3,4));
+		
+	//set the name for dialog
+	mainWindow->SetWindowName("State / Fit Assignment, Calibration, and Prediction");
+	//now add the overallOrg frame to this dialog
+	mainWindow->AddFrame(overallOrg, new TGLayoutHints(kLHintsExpandX | kLHintsExpandY,5,5,3,4));
 	//map the subwindows
 	mainWindow->MapSubwindows();
 	//init the layout engine
@@ -104,163 +185,71 @@ ParamAdjustmentDialog::ParamAdjustmentDialog(const TGWindow* parent, UInt_t widt
 	mainWindow->SetMWMHints(kMWMDecorAll | kMWMDecorResizeH  | kMWMDecorMaximize | kMWMDecorMinimize | kMWMDecorMenu,
                kMWMFuncAll |  kMWMFuncResize    | kMWMFuncMaximize | kMWMFuncMinimize,
                kMWMInputModeless);
-    
-    //map the main frame
+	//map the main frame
 	mainWindow->MapWindow();
+	mainWindow->Layout();
 }
 
-ParamAdjustmentDialog::~ParamAdjustmentDialog()
+AssignmentDialog::~AssignmentDialog()
 {
 	mainWindow->Cleanup();
+//	delete assignContainer;
+//	delete fitDataContainer;
+//	delete predictionContainer;
+	
 	delete mainWindow;
 }
 
-void ParamAdjustmentDialog::show()
+void AssignmentDialog::setStates(StateData* sts, int numSt)
 {
-	mainWindow->MapWindow();
-}
-
-void ParamAdjustmentDialog::hide()
-{
-	mainWindow->UnmapWindow();
-}
-
-void ParamAdjustmentDialog::clearPrevious()
-{
-	paramOrganizer->RemoveAll();
-	
-	if (paramNames != NULL)
-	{	delete[] paramNames;	}
-	
-	if (lowerBounds != NULL)
-	{	delete[] lowerBounds;	}
-
-	if (paramValues != NULL)
-	{	delete[] paramValues;	}
-	
-	if (upperBounds != NULL)
-	{	delete[] upperBounds;	}
-	
-	if (useBounds != NULL)
-	{	delete[] useBounds;	}
-	
-	if (paramFrames != NULL)
-	{	delete[] paramFrames;}
-}
-
-void ParamAdjustmentDialog::retrieveData(double* values, double* lowerBnds, double* upperBnds, bool* useBnds)
-{
-	for(int i=0; i<numParams; ++i)
+	numStates = numSt;
+	states = new StateData[numSt];
+	for(int i=0; i<numSt; ++i)
 	{
-		lowerBnds[i] = lowerBounds[i]->GetNumber();
-		values[i]    = paramValues[i]->GetNumber();
-		upperBnds[i] = upperBounds[i]->GetNumber();
-		useBnds[i]   = useBounds[i]->IsOn();
+		states[i] = sts[i];
 	}
-}
-
-void ParamAdjustmentDialog::updateParams(double* values)
-{
-	for(int i=0; i<numParams; ++i)
+	exEntry = new TGTextEntry*[numSt];
+	fpmEntry = new TGTextEntry*[numSt];
+	calfpmEntry = new TGTextEntry*[numSt];
+	aFitEntry = new TGTextEntry*[numSt];
+	difEntry = new TGTextEntry*[numSt];
+	
+	//create the columns
+	for(int i=0; i<numSt; ++i)
 	{
-		paramValues[i]->SetNumber(values[i]);
+		ostringstream namer;
+		namer.str("");
+		namer.clear();
+		exEntry[i] = new TGTextEntry(assignVert[0]);
+		exEntry[i]->SetEnabled(false);
+		namer<<sts[i].en<<" MeV";
+		exEntry[i]->SetText(namer.str().c_str(),false);
+		assignVert[0]->AddFrame(exEntry[i], new TGLayoutHints(kLHintsNormal, 1, 1, 1, 1));
+		
+		namer.str("");
+		namer.clear();
+		fpmEntry[i] = new TGTextEntry(assignVert[1]);
+		fpmEntry[i]->SetEnabled(false);
+		namer<<sts[i].fpMom<<" MeV/c";
+		fpmEntry[i]->SetText(namer.str().c_str(),false);
+		assignVert[1]->AddFrame(fpmEntry[i], new TGLayoutHints(kLHintsNormal, 1, 1, 1, 1));
+		
+		calfpmEntry[i] = new TGTextEntry(assignVert[2]);
+		calfpmEntry[i]->SetEnabled(false);
+		calfpmEntry[i]->SetText(namer.str().c_str(),false);
+		assignVert[2]->AddFrame(calfpmEntry[i], new TGLayoutHints(kLHintsNormal, 1, 1, 1, 1));
+		
+		aFitEntry[i] = new TGTextEntry(assignVert[3]);
+		aFitEntry[i]->SetEnabled(false);
+		assignVert[3]->AddFrame(aFitEntry[i], new TGLayoutHints(kLHintsNormal, 1, 1, 1, 1));
+		
+		difEntry[i] = new TGTextEntry(assignVert[4]);
+		difEntry[i]->SetEnabled(false);
+		assignVert[4]->AddFrame(difEntry[i], new TGLayoutHints(kLHintsNormal, 1, 1, 1, 1));
 	}
-}
-
-void ParamAdjustmentDialog::initWithParams(int numPeaks, int nP, double* vals)
-{
-	if(nP<(3*numPeaks))
-	{
-		cout<<"Error, fewer parameters than the number of peaks allows"<<endl;
-		return;
-	}
-	
-	int numBgParams=(nP-3*numPeaks);
-	
-	if( numBgParams>4 )
-	{
-		cout<<"Error, to many background parameters"<<endl;
-		return;
-	}
-	//set the number of parameters
-	numParams=nP;
-	clearPrevious();
-	
-	if(nP==0)
-	{ return; }
-	
-	paramFrames = new TGHorizontalFrame*[nP];
-	paramNames = new TGLabel*[nP];
-	lowerBounds = new TGNumberEntry*[nP];
-	paramValues = new TGNumberEntry*[nP];
-	upperBounds = new TGNumberEntry*[nP];
-	useBounds = new TGCheckButton*[nP];
-	
-	ostringstream nmr;
-	
-	//loop through each peak setting up its parameters
-	for(int i=0; i<numPeaks; ++i)
-	{
-		for(int j=0; j<3; ++j)
-		{
-			int ind = i*3 + j;
-			//create the frame
-			paramFrames[ind] = new TGHorizontalFrame(paramOrganizer, 10, 10);
-			//make the string for the param label
-			nmr.clear();
-			nmr.str("");
-			nmr<<GPNameList[j]<<i;
-			//make the parts of the frame
-			paramNames[ind] = new TGLabel(paramFrames[ind], nmr.str().c_str());
-			lowerBounds[ind] = new TGNumberEntry( paramFrames[ind], 0.5*vals[ind], 8, -1, TGNumberFormat::kNESRealOne,
-				TGNumberFormat::kNEAAnyNumber, TGNumberFormat::kNELNoLimits, 0.0, 1.0);
-			paramValues[ind] = new TGNumberEntry( paramFrames[ind], vals[ind], 8, -1, TGNumberFormat::kNESRealOne,
-				TGNumberFormat::kNEAAnyNumber, TGNumberFormat::kNELNoLimits, 0.0, 1.0);
-			upperBounds[ind] = new TGNumberEntry( paramFrames[ind], 2.0*vals[ind], 8, -1, TGNumberFormat::kNESRealTwo,
-				TGNumberFormat::kNEAAnyNumber, TGNumberFormat::kNELNoLimits, 0.0, 1.0);
-			useBounds[ind] = new TGCheckButton(paramFrames[ind], "Use Bnds?");
-			//add the parts to the frame
-			paramFrames[ind]->AddFrame(paramNames[ind], new TGLayoutHints(kLHintsNormal,5,5,3,4) );
-			paramFrames[ind]->AddFrame(lowerBounds[ind], new TGLayoutHints(kLHintsNormal,5,5,3,4) );
-			paramFrames[ind]->AddFrame(paramValues[ind], new TGLayoutHints(kLHintsNormal,5,5,3,4) );
-			paramFrames[ind]->AddFrame(upperBounds[ind], new TGLayoutHints(kLHintsNormal,5,5,3,4) );
-			paramFrames[ind]->AddFrame(useBounds[ind], new TGLayoutHints(kLHintsNormal,5,5,3,4) );
-			//add the frame to the organizer
-			paramOrganizer->AddFrame(paramFrames[ind], new TGLayoutHints(kLHintsNormal,5,5,3,4) );
-		}
-	}
-	
-	//now loop through the background setting it up its params
-	for(int i=0; i<numBgParams; ++i)
-	{
-		int ind = (3*numPeaks+i);
-		//create the frame
-		paramFrames[ind] = new TGHorizontalFrame(paramOrganizer, 10, 10);
-		//make the parts of the frame
-		paramNames[ind] = new TGLabel(paramFrames[ind], PBNameList[i]);
-		lowerBounds[ind] = new TGNumberEntry( paramFrames[ind], 0.5*vals[ind], 8, -1, TGNumberFormat::kNESRealTwo,
-			TGNumberFormat::kNEAAnyNumber, TGNumberFormat::kNELNoLimits, 0.0, 1.0);
-		paramValues[ind] = new TGNumberEntry( paramFrames[ind], vals[ind], 8, -1, TGNumberFormat::kNESRealTwo,
-			TGNumberFormat::kNEAAnyNumber, TGNumberFormat::kNELNoLimits, 0.0, 1.0);
-		upperBounds[ind] = new TGNumberEntry( paramFrames[ind], 2.0*vals[ind], 8, -1, TGNumberFormat::kNESRealTwo,
-			TGNumberFormat::kNEAAnyNumber, TGNumberFormat::kNELNoLimits, 0.0, 1.0);
-		useBounds[ind] = new TGCheckButton(paramFrames[ind], "Use Bnds?");
-		//add the parts to the frame
-		paramFrames[ind]->AddFrame(paramNames[ind], new TGLayoutHints(kLHintsNormal,5,5,3,4) );
-		paramFrames[ind]->AddFrame(lowerBounds[ind], new TGLayoutHints(kLHintsNormal,5,5,3,4) );
-		paramFrames[ind]->AddFrame(paramValues[ind], new TGLayoutHints(kLHintsNormal,5,5,3,4) );
-		paramFrames[ind]->AddFrame(upperBounds[ind], new TGLayoutHints(kLHintsNormal,5,5,3,4) );
-		paramFrames[ind]->AddFrame(useBounds[ind], new TGLayoutHints(kLHintsNormal,5,5,3,4) );
-		//add the frame to the organizer
-		paramOrganizer->AddFrame(paramFrames[ind], new TGLayoutHints(kLHintsNormal,5,5,3,4) );
-	}
-	mainWindow->SetWindowName("Parameter Adjuster");
-	//map the subwindows
 	mainWindow->MapSubwindows();
-	//init the layout engine
-	mainWindow->Resize(mainWindow->GetDefaultSize());
-	//map the main frame
 	mainWindow->MapWindow();
+	mainWindow->Layout();
 }
 
 #endif //ADJUST_DIALOGS_H
